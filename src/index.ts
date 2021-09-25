d20e834ceb2fcbadfdf966e06344b4468135cf5d
Roger Terrill <rogert@channelbakers.com>
2021-09-25 10:45:48 -0700

    Use Static Methods to consolidate presets

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import { MatchReader } from './MatchReader';
 import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+// const csvFileReader = new CsvFileReader('football.csv');
+//
+// // Create an instance of MatchReader and pass in something satisfying
+// // the 'DataReader' interface
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
